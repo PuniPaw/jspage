a5cf08dff157e9e25927da871a608742f7ba184e
PuniPaw <winstone1110@gmail.com>
2024-04-09 17:09:00 +0900

    create 1 file and update 2 files

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.getElementsByClassName("d")[0].style.color = 'gray';
+
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
